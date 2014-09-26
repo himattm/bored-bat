fc42464012ea1fd08a81dbe840a0d10fba8233da
Matt McKenna <matt.c.mckenna@gmail.com>
2014-09-25 21:51:30 -0400

    Finished warmup1: sumDouble with no conditionals!
    No sure about the use of indexOf(). This might use conditionals. Need to
    check the Java code.

--- warmup1/sumDouble.java
+++ warmup1/sumDouble.java
@@ -4,5 +4,14 @@
 */
 
 public int sumDouble(int a, int b) {
-	return a == b ? (a + b) *2 : a + b;
+	boolean equal = a == b; // Checks if the values are equal
+
+	String equalString = equal + ""; // makes the boolean a String
+
+	int num = equalString.indexOf("t") + 2; // Checks to see if the boolean contains "t"
+											// If it doenst it returns -1
+											// If it does returns 0 (the index of t)
+											// Increments by 2 to get 1 for false equals and 2 for true equals
+
+	return (a + b) * num;
 }
